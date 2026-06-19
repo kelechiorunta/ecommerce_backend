d6889b3fb488ee3e22d6736d4129e79ae0290538
Kelechi Orunta <kelechiorunta1@gmail.com>
2026-06-19 16:34:05 +0100

    feat(routes): implement customer routes

--- routes/customerRoutes.ts
+++ routes/customerRoutes.ts
@@ -1,9 +1,16 @@
 import { Router } from 'express';
-import { signin, signup } from '../controllers/customerController';
+import passport from 'passport';
+
+import { grantAccess, passportLogin, signin, signup } from '../controllers/customerController';
+import { passportLocalStrategy } from '../configs/passport/passport';
+import validateRedisSession from '../middleware/redisCacheMiddleware';
 
 const customerRouter = Router();
 
+passportLocalStrategy(passport);
+
 customerRouter.post('/signup', signup);
-customerRouter.post('/signin', signin);
+customerRouter.post('/signin', passportLogin);
+customerRouter.get('/isAuthenticated', validateRedisSession, grantAccess);
 
 export default customerRouter;

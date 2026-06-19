e8d5622ea6e7485e121de0d229bdd01044e9a7e3
Kelechi Orunta <kelechiorunta1@gmail.com>
2026-06-19 16:36:01 +0100

    feat(app): implement third party middlewares

--- app.ts
+++ app.ts
@@ -1,16 +1,23 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import session from 'express-session';
+import sessionStorage from './configs/redis/redisStore';
 
 import productRouter from './routes/productRoutes';
 import categoryRouter from './routes/categoryRoutes';
 import producerRouter from './routes/producerRoutes';
 import customerRouter from './routes/customerRoutes';
+import passport from 'passport';
 
 const app = express();
 
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(sessionStorage);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/api/products', productRouter);
 app.use('/api/categories', categoryRouter);

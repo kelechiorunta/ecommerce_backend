0eb464acc2c8cba405b2ca3a52684547ba481c1c
Kelechi Orunta <kelechiorunta1@gmail.com>
2026-06-19 01:56:24 +0100

    feat(app): add express middleawares for form data body parsing

--- app.ts
+++ app.ts
@@ -1,12 +1,20 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+
 import productRouter from './routes/productRoutes';
 import categoryRouter from './routes/categoryRoutes';
 import producerRouter from './routes/producerRoutes';
+import customerRouter from './routes/customerRoutes';
 
 const app = express();
+
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/api/products', productRouter);
 app.use('/api/categories', categoryRouter);
 app.use('/api/producers', producerRouter);
+app.use('/api/customers', customerRouter);
 
 export default app;

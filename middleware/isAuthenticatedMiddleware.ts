764852629b288544bea8d70363af29d4de798090
Kelechi Orunta <kelechiorunta1@gmail.com>
2026-06-19 16:32:15 +0100

    feat(middleware): implement redis cache and authentication middleware

---
+++ middleware/isAuthenticatedMiddleware.ts
@@ -0,0 +1,19 @@
+import type { Request, Response, NextFunction } from 'express';
+
+export const checkAuthenticate = (req: Request, res: Response, next: NextFunction) => {
+  try {
+    // console.log('Authenticated?', req.isAuthenticated());
+    // console.log('Session:', req.session);
+    // console.log('User:', req.user);
+
+    // Check if user is authenticated with Passport
+    if (!req.isAuthenticated() || !req.user) {
+      console.log('FAILED MIDDLEWARE AUTHENTICATION');
+      // This is the login route in the frontend
+      return res.redirect('/login');
+    }
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
---
+++ middleware/redisCacheMiddleware.ts
@@ -0,0 +1,43 @@
+import type { Request, Response, NextFunction } from 'express';
+
+import redisClient from '../configs/redis/redis';
+
+const validateRedisSession = async (req: any, res: Response, next: NextFunction) => {
+  try {
+    // Ensure session exists
+    if (!req.session?.user?.email) {
+      return res.status(401).json({
+        error: 'No active session'
+      });
+    }
+
+    const cacheKey = `customer:${req.session.user.email}`;
+
+    console.log('CACHE KEY:', cacheKey);
+
+    // Fetch from Redis
+    const cachedUser = await redisClient.get(cacheKey);
+
+    if (!cachedUser) {
+      return res.status(404).json({
+        error: 'No redis cache available'
+      });
+    }
+
+    const parsedUser = JSON.parse(cachedUser);
+
+    // Populate request
+    // req.session.user = parsedUser;
+    req.user = parsedUser;
+
+    next();
+  } catch (error) {
+    console.error(error);
+
+    return res.status(500).json({
+      error: 'Redis session validation failed'
+    });
+  }
+};
+
+export default validateRedisSession;

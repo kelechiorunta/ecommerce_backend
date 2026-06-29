aa1dc8f59cc93469b28dbaf95dadbf4a1cdac342
Kelechi Orunta <kelechiorunta1@gmail.com>
2026-06-29 23:17:37 +0100

    fix(routes): fix ordering of the routes

--- routes/customerRoutes.ts
+++ routes/customerRoutes.ts
@@ -11,14 +11,17 @@ import {
 import { passportLocalStrategy } from '../configs/passport/passport';
 import validateRedisSession from '../middleware/redisCacheMiddleware';
 import { checkAuthenticate } from '../middleware/isAuthenticatedMiddleware';
+import redisClient from '../configs/redis/redis';
 
 const customerRouter = Router();
+
+redisClient.connect().catch((err) => console.error(err instanceof Error ? err.message : err));
 
 passportLocalStrategy(passport);
 
 customerRouter.post('/signup', signup);
 customerRouter.post('/signin', passportLogin);
 customerRouter.get('/logout', logout);
-customerRouter.get('/isAuthenticated', checkAuthenticate, validateRedisSession, grantAccess);
+customerRouter.get('/isAuthenticated', validateRedisSession, grantAccess);
 
 export default customerRouter;

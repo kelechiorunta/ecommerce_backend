30e2df77c04cb63feab28769c575d71d5e0c87f2
Kelechi Orunta <kelechiorunta1@gmail.com>
2026-08-18 18:03:17 +0100

    migrations: implement payment history table

---
+++ migrations/20260816032213_add_payment_history_table.ts
@@ -0,0 +1,15 @@
+import type { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable('payment_history', (table) => {
+    table.increments('payment_history_id').notNullable();
+    table.integer('customer_id').references('customer_id').inTable('customer');
+    table.text('report');
+    table.string('status');
+    table.timestamps(true, true);
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable('payment_history');
+}
